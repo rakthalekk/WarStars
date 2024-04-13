cb1cf137b28caff77910b48e0fc03b5dad300bda
CharlieLomax <charlielomax2000@gmail.com>
2024-04-13 17:55:04 -0400

    Updated equipment check type

--- src/Person.gd
+++ src/Person.gd
@@ -1,17 +1,17 @@
 extends Node2D
 class_name Person
 
-var weapon1 : Node2D
-var weapon2 : Node2D
+var weapon1 : Equipment
+var weapon2 : Equipment
 
-var equip1 : Node2D
-var equip2 : Node2D
+var equip1 : Equipment
+var equip2 : Equipment
 
 @export var base_price = 100
 @export var weapon_price_base = 25
-@export var price = 100
+var price = 100
 
-var _tier = 1
+var tier = 1
 @export var health = 8
 @export var speed = 10
 
@@ -22,7 +22,7 @@ var _tier = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	price 
+	price = base_price
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -32,39 +32,39 @@ func _process(delta):
 func raise_tier_to(new_tier : int):
 	if new_tier > max_tier:
 		new_tier = max_tier
-	if new_tier < _tier:
+	if new_tier < tier:
 		return
 	
-	while(_tier < new_tier):
-		_tier += 1
+	while(tier < new_tier):
+		tier += 1
 		health += randi_range(min_health_gain, max_health_gain)
 		price += base_price
 
 # Raise tier by 1
 func raise_tier():
-	raise_tier_to(_tier + 1)
+	raise_tier_to(tier + 1)
 
-func equip(slot : int, equipment : Node2D):
+func equip(slot : int, equipment : Equipment):
 	match slot:
 		0:
 			# Check that it's a weapon, otherwise return
-			#if equipment is Weapon:
-			weapon1 = equipment
-			#else: return
+			if equipment.equip_type == Equipment.Equip_Type.WEAPON:
+				weapon1 = equipment
+			else: return
 		1:
 			# Check that it's a weapon, otherwise return
-			#if equipment is Weapon:
-			weapon2 = equipment
+			if equipment.equip_type == Equipment.Equip_Type.WEAPON:
+				weapon2 = equipment
 			#else: return
 		2:
 			# Check that it's not a weapon, otherwise return
-			#if not equipment is Weapon:
-			equip1 = equipment
-			#else: return
+			if not equipment.equip_type == Equipment.Equip_Type.WEAPON:
+				equip1 = equipment
+			else: return
 		3:
 			# Check that it's not a weapon, otherwise return
-			#if not equipment is Weapon:
-			equip2 = equipment
-			#else: return
+			if not equipment.equip_type == Equipment.Equip_Type.WEAPON:
+				equip2 = equipment
+			else: return
 	# Call equipment's stat change function
 	price += equipment.tier * weapon_price_base
