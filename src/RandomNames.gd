364dac98a84e330e1787d38dd33e562cc446b5b9
Haley Barnette <henry.barnette@gmail.com>
2024-04-14 00:46:42 -0400

    Tested Updated Alien Name List

--- src/RandomNames.gd
+++ src/RandomNames.gd
@@ -16,13 +16,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var name = get_random_name()
-	if (name != ""):
-		print(name)
+	#var name = get_random_name()
+	#if (name != ""):
+		#print(name)
+	pass
 
 func get_random_name() -> String:
-	if (alienList.size() == 0):
-		return ""
 	var rng = RandomNumberGenerator.new()
 	if (rng.randi_range(1, 4192) == 1):
 		if (devList.size() == 0):
@@ -32,6 +31,8 @@ func get_random_name() -> String:
 		var devElement = devList[devIndex]
 		devList.remove_at(devIndex)
 		return devElement
+	if (alienList.size() == 0):
+		return ""
 	var alienIndex = rng.randi_range(0, alienList.size() - 1)
 	var alienElement = alienList[alienIndex]
 	alienList.remove_at(alienIndex)
