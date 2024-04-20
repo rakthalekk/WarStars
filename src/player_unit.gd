b494736b4530fdd8b8e1e12d54cb29242194e13c
Jack <blazejmg917@gmail.com>
2024-04-20 18:28:28 -0400

    made it so unit and equip visuals work and persisy

--- src/player_unit.gd
+++ src/player_unit.gd
@@ -3,10 +3,12 @@ extends Unit
 
 
 var hex_colors = [Color("8cff9b"), Color("ffaa6e"), Color("c37289"), Color("ffe091"), Color("ffa5d5"), Color("78fae6"), Color("b483ef"), Color("ff6675")]
+var assigned_sprites: bool = false
 
 func _ready():
 	super()
-	select_sprites()
+	if(!assigned_sprites):
+		select_sprites()
 
 
 func select_sprites():
@@ -21,3 +23,16 @@ func select_sprites():
 	var color = hex_colors.pick_random()
 	$PathFollow2D/Head.modulate = color
 	$PathFollow2D/Hands.modulate = color
+	assigned_sprites = true
+
+func set_sprites(head: Texture, color: Color):
+	$PathFollow2D/Head.texture = head
+	print("head type: " + head.get_path() + " to " + head.get_path().get_file())
+	if head.get_path().get_file() == "UnitHeadType1.png":
+		$PathFollow2D/Helmet.show()
+	elif head.get_path().get_file() == "UnitHeadType5.png":
+		$PathFollow2D/Head.hframes = 2
+		idle_anim = "head5_idle"
+	$PathFollow2D/Head.modulate = color
+	$PathFollow2D/Hands.modulate = color
+	assigned_sprites = true
