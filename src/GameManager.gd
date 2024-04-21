9f1c16e5344e294d04d1077432584a1ccd9c5d4d
dmglynn <dmglynn@ncsu.edu>
2024-04-21 19:54:32 -0400

    some equipment fixes and stuff

--- src/GameManager.gd
+++ src/GameManager.gd
@@ -91,6 +91,8 @@ func load_fleet(units: Array[Unit] = []):
 		unit.person_source.update_from_unit(unit)
 	var main = get_tree().root.get_node("Main")
 	get_tree().root.remove_child(combat_scene)
+	combat_scene.queue_free()
+	combat_scene = null
 	get_tree().root.add_child(fleet)
 		
 func enter_combat(platoon: Array[Person_Icon]):
@@ -102,6 +104,8 @@ func enter_combat(platoon: Array[Person_Icon]):
 		get_combat_scene().get_node("GameBoard").add_child(unit)
 		
 	get_tree().root.remove_child(fleet)
+	fleet.queue_free()
+	fleet = null
 	get_tree().root.add_child(combat_scene)
 
 
